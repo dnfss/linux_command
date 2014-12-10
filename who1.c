7ca942e5703767d1a4b6452821a970605616b35d
dnfss <qkhhdnfss@gmail.com>
2014-12-10 21:49:21 +0800

    worked version for suse

--- who1.c
+++ who1.c
@@ -1,27 +1,41 @@
 
+// learns how to covert seconds to human readable format. (with function 'ctime')
+//
 #include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
 #include "fcntl.h"
-#include "utmpx.h"
+#include "utmp.h"
+#include "time.h"
 
 #define SHOWHOST
 
-void show_info(struct utmpx *record) {
-	printf("%-8.8s ", record->ut_user);
-	printf("%-8.8s ", record->ut_line);
+void show_info(struct utmp *record) {
+	if( record->ut_type != USER_PROCESS ) {
+		return;
+	}
+
+	printf("%-15.15s ", record->ut_user);
+	printf("%-15.15s ", record->ut_line);
+
+	time_t t = (time_t)(record->ut_tv.tv_sec);
+	printf("%20.20s ", ctime(&t) + 4);
+
+#ifdef SHOWHOST
+	printf("(%s)", record->ut_host);
+#endif
+	printf("\n");
 }
 
 int main(int argc, char *argv[]) {
 	int utmpfd;
-	struct utmpx current_record;
-	int reclen = sizeof(current_record);
-
-	if( (utmpfd = open(UTMPX_FILE, O_RDONLY)) == -1 ) {
-		perror(UTMPX_FILE);
+	struct utmp current_record;
+	if( (utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 ) {
+		perror(UTMP_FILE);
 		exit(-1);
 	}
 
+	int reclen = sizeof(current_record);
 	while( read(utmpfd, &current_record, reclen) == reclen ) {
 		show_info(&current_record);
 	}
