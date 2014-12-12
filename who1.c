234755abf1c5e94758a2d5bea644e0bdf5c7b736
dnfss <qkhhdnfss@gmail.com>
2014-12-12 19:49:48 +0800

    version 1.0

--- who1.c
+++ who1.c
@@ -1,14 +1,57 @@
 
-// learns how to covert seconds to human readable format. (with function 'ctime')
-//
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "fcntl.h"
-#include "utmp.h"
-#include "time.h"
+/*
+ * Note
+ *	 covert seconds to human readable format. (with function 'ctime')
+ *	 reduce system call with carton
+ *	 This implementation is not thread safe
+ */
+
+#include <utmp.h>
+#include <time.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define SHOWHOST
+#define MAX_RECORDS 16
+
+static int curIdx = 0;
+static int utmp_fd = -1;
+static int recordCnt = 0;
+static char utmpbuf[MAX_RECORDS * sizeof(struct utmp)] = {0};
+
+int utmp_open(const char *filename) {
+	utmp_fd = open(filename, O_RDONLY);
+	curIdx = 0;
+	recordCnt = 0;
+	return utmp_fd;
+}
+
+// This function is not thread safe
+struct utmp* utmp_next() {
+	if( utmp_fd == -1 ) {
+		return NULL;
+	}
+
+	if( curIdx == recordCnt ) {	// reload
+		int readSize = read(utmp_fd, utmpbuf, MAX_RECORDS * sizeof(struct utmp));
+		if( (recordCnt = readSize / sizeof(struct utmp)) == 0 ) {
+			return NULL;
+		}
+
+		curIdx = 0;
+	}
+
+	return (struct utmp*)(&utmpbuf[curIdx++ * sizeof(struct utmp)]);
+}
+
+void utmp_close() {
+	if( utmp_fd != -1 ) {
+		close(utmp_fd);
+		utmp_fd = -1;
+	}
+}
 
 void show_info(struct utmp *record) {
 	if( record->ut_type != USER_PROCESS ) {
@@ -28,18 +71,16 @@ void show_info(struct utmp *record) {
 }
 
 int main(int argc, char *argv[]) {
-	int utmpfd;
-	struct utmp current_record;
-	if( (utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 ) {
+	if( utmp_open(UTMP_FILE) == -1 ) {
 		perror(UTMP_FILE);
 		exit(-1);
 	}
 
-	int reclen = sizeof(current_record);
-	while( read(utmpfd, &current_record, reclen) == reclen ) {
-		show_info(&current_record);
+	struct utmp *current_record;
+	while( (current_record = utmp_next()) != NULL ) {
+		show_info(current_record);
 	}
 
-	close(utmpfd);
+	utmp_close();
 	return 0;
 }
